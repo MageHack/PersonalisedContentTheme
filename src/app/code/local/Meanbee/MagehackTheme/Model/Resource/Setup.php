1c7a5bf2d0de55663e8a931c000e8b6232618409
Ash Smith <ash.smith@meanbee.com>
2015-08-30 10:46:49 +0100

    rename images

--- src/app/code/local/Meanbee/MagehackTheme/Model/Resource/Setup.php
+++ src/app/code/local/Meanbee/MagehackTheme/Model/Resource/Setup.php
@@ -2,6 +2,17 @@
 
 class Meanbee_MagehackTheme_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
 {
+    /**
+     * Update an existing CMS block.
+     *
+     * @param string $identifier
+     * @param string $title
+     * @param string $content
+     * @param array  $personalisation_tags  array of category ids
+     * @param array  $stores  Store the block is currently assigned to
+     *
+     * @return $this
+     */
     public function createPersonalisationCmsBlock($identifier, $title, $content, $personalisation_tags = array(), $stores = array(0))
     {
         $current_store = Mage::app()->getStore();
@@ -16,6 +27,35 @@ class Meanbee_MagehackTheme_Model_Resource_Setup extends Mage_Core_Model_Resourc
                 "stores"     => $stores
             ))
             ->save();
+        Mage::app()->setCurrentStore($current_store);
+        return $this;
+    }
+
+    /**
+     * Update an existing CMS page.
+     *
+     * @param string   $identifier
+     * @param array    $data    A key => value list of new data to assign to the page.
+     * @param int|null $storeId Store ID the page is currently assigned to.
+     *
+     * @return $this
+     */
+    public function updateCmsPage($identifier, $data, $storeId = null)
+    {
+        $current_store = Mage::app()->getStore();
+        Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+        $page = Mage::getModel("cms/page");
+        if ($storeId) {
+            $page->setStoreId($storeId);
+        }
+        $page->load($identifier);
+        if ($page->getId()) {
+            $page
+                ->addData($data)
+                ->save();
+        } else {
+            Mage::throwException(sprintf("Unable to update CMS page: CMS page '%s' does not exist.", $identifier));
+        }
         Mage::app()->setCurrentStore($current_store);
         return $this;
     }
